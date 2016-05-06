09f1ab54f7ea68370e95bf307c28200a95015599
tperi-test <tamil@narwhal.digital>
2016-05-06 14:06:42 -0400

    updated to end animation at last image

--- cycler.js
+++ cycler.js
@@ -1,14 +1,16 @@
 function cycleImages() {
     var $active = $('#nd-cycler .active');
     var $next = ($('#nd-cycler .active').next().length > 0) ? $('#nd-cycler .active').next() : $('#nd-cycler img:first');
+    var $end = $('#nd-cycler img#end');
     $next.css('z-index', 2);
     $active.fadeOut(1500, function () {
         $active.css('z-index', 1).show().removeClass('active');
         $next.css('z-index', 3).addClass('active');
     });
+    $end.stop();
 }
 
 $(document).ready(function () {
     $('#nd-cycler').fadeIn(1000);
     setInterval('cycleImages()', 4000);
-})
+})  
