eae8c3c75088ddf901acae47668b0e32243c9c3a
tperi-test <tamil@narwhal.digital>
2016-05-10 04:55:54 -0400

    Full update to v3

--- cycler.js
+++ cycler.js
@@ -1,8 +1,8 @@
-var $timer = 900;
+var $timer = 450;
 
 function cycleImages() {
     var $active = $('#nd-cycler .active');
-    var $next = ($('#nd-cycler .active').next().length > 0) ? $('#nd-cycler .active').next() : $('#nd-cycler img:first');
+    var $next = ($active.next().length > 0) ? $active.next() : $('#nd-cycler img:first');
     var $end = $('#nd-cycler img#end');
     $next.css('z-index', 2);
     $active.fadeOut($timer, function () {
@@ -14,5 +14,21 @@ function cycleImages() {
 
 $(document).ready(function () {
     $('#nd-cycler').fadeIn($timer);
-    setInterval('cycleImages()', 1500);
-})  
+    setInterval('cycleImages()', $timer);
+});
+
+$(function () {
+    var string = "NATIVE";
+    var q = jQuery.map(string.split(''), function (letter) {
+        return $('<span>' + letter + '</span>');
+    });
+
+    var dest = $('.nd-msg');
+
+    var c = 0;
+    var i = setInterval(function () {
+        q[c].appendTo(dest).hide().fadeIn($timer);
+        c += 1;
+        if (c >= q.length) clearInterval(i);
+    }, $timer);
+});
