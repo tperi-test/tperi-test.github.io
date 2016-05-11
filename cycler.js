31d2368bc448fdcbfb93a16be6c4c03746a03a48
tperi-test <tamil@narwhal.digital>
2016-05-11 11:08:28 -0500

    Add files via upload

--- cycler.js
+++ cycler.js
@@ -11,39 +11,21 @@ function cycleImages() {
     });
     $end.stop();
 }
-function cycleBackground() {
-    var $div = $('#nd-cycler');
-    var $image = new Array [
-        "images/bg/bg-n.jpg",
-        "images/bg/bg-a.jpg"
-    ];
-    $div.fadeOut($timer, function () {
-       $div.css('background') 
-    });
-    
-}
+/*
+ function cycleBackground() {
+ var $div = $('#nd-cycler');
+ var $image = new Array [
+ "images/bg/bg-n.jpg",
+ "images/bg/bg-a.jpg"
+ ];
+ $div.fadeOut($timer, function () {
+ $div.css('background') 
+ });
+
+ }
+ */
 
 $(document).ready(function () {
     $('#nd-cycler').fadeIn($timer);
     setInterval('cycleImages()', $timer);
 });
-
-/*
-
-$(function () {
-    var string = "NATIVE";
-    var q = jQuery.map(string.split(''), function (letter) {
-        return $('<span>' + letter + '</span>');
-    });
-
-    var dest = $('.nd-msg');
-
-    var c = 0;
-    var i = setInterval(function () {
-        q[c].appendTo(dest).hide().fadeIn($timer);
-        c += 1;
-        if (c >= q.length) clearInterval(i);
-    }, $timer);
-});
-
-    */
